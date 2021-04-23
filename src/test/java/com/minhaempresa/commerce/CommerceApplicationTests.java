b53026a0690ee12b20095a5ded82a457539f2c58
http murilo <muriloeduardo1997@outlook.com>
2021-04-23 17:18:41 -0300

    first commit

---
+++ src/test/java/com/minhaempresa/commerce/CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaempresa.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
